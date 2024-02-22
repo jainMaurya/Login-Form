6e5f67afa7e3b0f5770c0dc598384f1d2d48346e
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-02-22 14:14:46 +0000

    basic form

--- form_maker/models.py
+++ form_maker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+# class Form(models.Model):
+#     # name=models.CharField(max_length=100)
+#     # rollno=models.IntegerFeild()
--- form_maker/views.py
+++ form_maker/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def  index(request):
+    # data=request.POST()
+    # Name=data.get('name')
+    # Rollno=data.get('rollno')
     return render(request,"index.html")
